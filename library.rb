9d79e1f6159323946e9744253bf725b2c856454d
Joslyn Rosbrook <jrosbrook@gmail.com>
2013-12-04 22:17:54 -0800

    updated methods/example outputs

--- library.rb
+++ library.rb
@@ -5,15 +5,23 @@ class Library
 	end	
 
 	def addshelf(shelf)
-		@shelf.push(shelf)
+		@shelves.push(shelf)
 	end
 
 	def removeshelf(shelf)
-		@shelf.delete(shelf)
+		@shelves.delete(shelf)
 	end
 
 	def shelvescount()
 		@shelves.length
+	end
+
+	def reportbooks
+		@shelves.each do |shelf|
+			shelf.books each do |book|
+              book.title
+			end
+		end
 	end
 end
 
@@ -30,9 +38,21 @@ class Shelf
 	def removebook(book)
 		@books.delete(book)
 	end
+
+	def books 
+		@books
+	end
 end
 
 class Book 
+
+	def initialize (title)
+		@title = title
+	end 
+
+	def title 
+		@title 
+	end
 
 	def enshelf (shelf)
 		shelf.addbook(self)
@@ -43,3 +63,34 @@ class Book
 	end	
 
 end
+
+b1 = Book.new("Ender's Game")
+b2 = Book.new("Game of Thrones")
+b3 = Book.new("The Golden Compass")
+
+lib = Library.new
+
+s1 = Shelf.new
+s2 = Shelf.new 
+s3 = Shelf.new
+
+b1.enshelf(s1)
+b2.enshelf(s2)
+b3.enshelf(s2)
+
+lib.addshelf(s1)
+lib.addshelf(s2)
+lib.addshelf(s3)
+
+lib.removeshelf(s3)
+
+puts lib.shelvescount
+
+puts lib.reportbooks
+
+b1.unshelf(s1)
+
+lib.reportbooks
+
+puts "done"
+
